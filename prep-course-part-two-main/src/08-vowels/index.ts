dd2a7cceb41fffe5a6932385b3b0364d8df85247
Artis Vansovics <vansovics.artis@gmail.com>
2022-03-16 16:52:17 +0000

    commit to 'master'

--- prep-course-part-two-main/src/08-vowels/index.ts
+++ prep-course-part-two-main/src/08-vowels/index.ts
@@ -9,10 +9,10 @@
  * vowels('Hello there!') === 4
  */
 
-const isAVowel: string[] = ['a', 'e', 'i', 'o', 'u']
+const isAVowel = ['a', 'e', 'i', 'o', 'u']
 
 function vowels(s: string) {
-    const cleanString: string = (s.replace("'", "").replace(/[^A-Za-z]/g, " ")).toLowerCase()
+    const cleanString: string = (s.replace(/[^A-Za-z]/g, " ")).toLowerCase()
     const splitString: string[] = cleanString.split('')
     const vowelsInString: string[] = splitString.filter(n => isAVowel.includes(n)) // filters any value from string that is the same value as values in isAVowel
     return vowelsInString.length
